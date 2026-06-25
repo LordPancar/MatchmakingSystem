e79d686c2c8ff8d220c322ed55b51f37621585c2
LordPancar <sancarerdogan@ogr.eskisehir.edu.tr>
2026-06-25 14:50:56 +0300

    controller update

--- Matchmaking.Api/Controllers/MatchmakingController.cs
+++ Matchmaking.Api/Controllers/MatchmakingController.cs
@@ -1,8 +1,29 @@
 ﻿using MassTransit;
-using Matchmaking.S
-namespace Matchmaking.Api.Controllers
+using Matchmaking.Shared;
+using MatchMaking.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace Matchmaking.Api.Controllers;
+
+[ApiController]
+[Route("api/matchmaking")]
+public class MatchmakingController : ControllerBase
 {
-    public class MatchmakingController
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public MatchmakingController(IPublishEndpoint publishEndpoint)
     {
+        _publishEndpoint = publishEndpoint; 
     }
+
+    [HttpPost("queue")]
+    public async Task<IActionResult> QueueRequest([FromBody] MatchRequest request)
+    {
+        await _publishEndpoint.Publish(request);
+        return Accepted(new { message = "Kuyruğa alındı.", requestId = request.RequestId });
+    }
+
+
 }
+
