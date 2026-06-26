8ed039bd0d35c6a26818e9cfa0d3cbac53a35a68
LordPancar <sancarerdogan@ogr.eskisehir.edu.tr>
2026-06-26 09:33:57 +0300

    Matchmaking.Worker MatchRequestConsumer and Program update

---
+++ Matchmaking.Worker/MatchRequestConsumer.cs
@@ -0,0 +1,35 @@
+﻿
+using MassTransit;
+using Matchmaking.Shared;
+using MatchMaking.Shared;
+
+namespace Matchmaking.Worker;
+
+public class MatchRequestConsumer : IConsumer<MatchRequest>
+{
+    private readonly ILogger<MatchRequestConsumer> _logger;
+    private readonly MatchmakingEngine _engine;
+    public MatchRequestConsumer(ILogger<MatchRequestConsumer> logger, MatchmakingEngine engine)
+    {
+        _logger = logger;
+        _engine = engine;
+    }
+
+    public async Task Consume(ConsumeContext<MatchRequest> context)
+    {
+        var request = context.Message;
+        _logger.LogInformation("Mesaj alındı: {UserId}, Skor: {Score}", request.UserId, request.Score);
+
+        _engine.AddToQueue(request);
+        var matches = _engine.TryMatch();
+
+        foreach (var match in matches)
+        {
+            _logger.LogInformation("Eşleşme bulundu: {P1} vs {P2} (fark: {diff})", match.Player1Id, match.Player2Id, match.ScoreDifference);
+
+        }
+
+    }
+}
+
+
--- Matchmaking.Worker/Program.cs
+++ Matchmaking.Worker/Program.cs
@@ -1,7 +1,25 @@
+using MassTransit;
+using Matchmaking.Shared;
 using Matchmaking.Worker;
+using MatchMaking.Shared;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+builder.Services.AddSingleton<MatchmakingEngine>();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<MatchRequestConsumer>();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var host = builder.Build();
 host.Run();
